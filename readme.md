f4e0cdc9efeb2324d39e68cf60f5c92b8a05b9ad
Brian Green <dear.briangreen@gmail.com>
2019-03-05 23:39:32 -0800

    first commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Simple Console Quiz
+Load the HTML file, open the console, and enter the right answer's number in the prompt.
+
+Reload page to get a new question. 
---
+++ script.js
@@ -0,0 +1,34 @@
+(function(){
+  function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+  }
+
+  Question.prototype.quizTime =  function(){
+    console.log(this.question)
+    for (var i = 0; i < this.answers.length; i++) {
+      console.log(i+1 + ': ' + this.answers[i]);
+    }
+    this.player_choice = window.prompt("Pick the number of the right answer");
+  };
+
+  Question.prototype.quizCheck = function(){
+    if (this.correct === (parseInt(this.player_choice))) {
+      console.log("Correct!")
+    } else {
+      console.log("Wrong.")
+    }
+  }
+
+  var question1 = new Question('Who has hit the most home runs?',['Barry Bonds','Hank Aaron','Mark McGwire'],1);
+  var question2 = new Question('What is the capital of Spain?',['Barcelona','Madrid','Catalonia'],2);
+  var question3 = new Question('What is the first inert gas?',['Xenon','Argon','Helium'],3);
+
+  var questionsArray = [question1,question2,question3];
+
+  var n = Math.floor(Math.random()*questionsArray.length);
+
+  questionsArray[n].quizTime();
+  questionsArray[n].quizCheck();
+})();
